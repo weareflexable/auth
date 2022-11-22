0ca47b0cd8adeac8ce78517930c9b0b41c5a2d92
mujeex <mujahid.bappai@yahoo.com>
2022-11-22 22:01:41 +0100

    choir: add random space

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosApp = axios.create({
-    // baseURL: process.env.NEXT_PUBLIC_API_URL
+    // baseURL: process.env.NEXT_PUBLIC_API_URL 
     baseURL: 'https://platform.flexabledats.com/api/v1.0/'
 })
