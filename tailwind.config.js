e2ffa11b96ed163b8b33c22daf4f19acd19cd05c
ss251 <ss251@uw.edu>
2022-12-31 04:29:57 +0530

    feat: redesign login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      figtree: ["Figtree"],
+    },
     extend: {
       colors: {
         primary: "#010B14",
