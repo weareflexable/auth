3848f76678b61a4b6876b5523762fb99dcd8b655
mbappai <mujahid.bappai@yahoo.com>
2024-11-14 21:46:08 +0100

    refactor: hardcode supasekey

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -3,7 +3,8 @@ import { createClient } from "@supabase/supabase-js";
 const supabase = createClient(
   // process.env.NEXT_PUBLIC_SUPABASE_URL,
   'https://axwqqoifubxkipmilrmm.supabase.co',
-  process.env.NEXT_PUBLIC_SUPABASE_API_KEY
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4d3Fxb2lmdWJ4a2lwbWlscm1tIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzcyMzQzMzEsImV4cCI6MTk5MjgxMDMzMX0.hXagLstTQVOeyM3sJYxTLrL2SwBF9mgjrGI8Us8A6a4'
+  // process.env.NEXT_PUBLIC_SUPABASE_API_KEY
 );
 
 export default supabase;
