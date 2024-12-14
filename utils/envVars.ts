f24eecb904f54da870234581ed3b8015815d7f88
mbappai <mujahid.bappai@yahoo.com>
2024-12-14 16:42:37 +0100

    refactor: use dev values

--- utils/envVars.ts
+++ utils/envVars.ts
@@ -1,9 +1,10 @@
-const NEXT_PUBLIC_SUPABASE_URL = 'https://sqfhktjlzezxxfvypqiu.supabase.co'
-const NEXT_PUBLIC_SUPABASE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxZmhrdGpsemV6eHhmdnlwcWl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjM5NTM5NzgsImV4cCI6MTk3OTUyOTk3OH0.RCLswqFgQm0VHQj-z6CJX5QP_dL0GKY7_wXPGzKmOfU'
-const NEXT_PUBLIC_ORIGIN = 'https://auth.flexabledats.com'
-const NEXT_PUBLIC_API_URL = 'https://platform.flexabledats.com/api/v1.0/'
-const NEXT_PUBLIC_MARKETPLACE = 'https://marketplace.flexabledats.com'
-const NEXT_PUBLIC_PORTAL = 'https://portal.flexabledats.com'
+const NEXT_PUBLIC_SUPABASE_URL = 'https://axwqqoifubxkipmilrmm.supabase.co'
+const NEXT_PUBLIC_SUPABASE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4d3Fxb2lmdWJ4a2lwbWlscm1tIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzcyMzQzMzEsImV4cCI6MTk5MjgxMDMzMX0.hXagLstTQVOeyM3sJYxTLrL2SwBF9mgjrGI8Us8A6a4'
+const NEXT_PUBLIC_ORIGIN = 'https://auth.dev.flexabledats.com'
+const NEXT_PUBLIC_API_URL = 'https://platform.dev.flexabledats.com/api/v1.0/'
+const NEXT_PUBLIC_MARKETPLACE = 'https://marketplace.dev.flexabledats.com'
+const NEXT_PUBLIC_PORTAL = 'https://portal.dev.flexabledats.com'
+const NEXT_PUBLIC_RECAPTCHA_SITE_KEY = '6LfIi1InAAAAAJDDvZ6cIUuRX8ZpXKXaXJKvZ-V7'
 
 const envUtils = {
     NEXT_PUBLIC_SUPABASE_URL,
@@ -11,7 +12,8 @@ const envUtils = {
     NEXT_PUBLIC_ORIGIN,
     NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_MARKETPLACE,
-    NEXT_PUBLIC_PORTAL
+    NEXT_PUBLIC_PORTAL,
+    NEXT_PUBLIC_RECAPTCHA_SITE_KEY
 }
 
 export default envUtils
